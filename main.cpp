d1e80cd4e9ebf67e0adbfdf4abe4689c1ba8e4dd
medc-0 <moonisontop76@gmail.com>
2025-11-14 15:39:46 +0100

    feat: learned type conversion and quick fix

--- main.cpp
+++ main.cpp
@@ -79,11 +79,31 @@ int main() {
 
     int remainder = students % 2;
 
-    parenthesis
-    multiplication & Division
-    addition & subtraction
+    // parenthesis
+    // multiplication & Division
+    // addition & subtraction
 
     std::cout << "Students: " << students;
+    */
+
+    // type conversion = conversion a value of one data type to another
+    //                   Implicit = automatic
+    //                   Explicit = Precede value with new data type (int)
+
+    /*
+    double x = (int)3.14;
+
+    char c = 100;
+
+    std::cout << c;
+
+    std::cout << (char)100; // returns letter 'd' in ascii
+
+    int correct = 8;
+    int questions = 10;
+    double score = correct / (double)questions * 100;
+
+    std::cout << score << "%";
     */
 
     return 0; 
