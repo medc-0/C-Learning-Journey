9727ac56f4d547bbc5b410479024888a68be7045
medc-0 <moonisontop76@gmail.com>
2025-11-14 16:41:00 +0100

    docs: learned, nullptrs

--- main.cpp
+++ main.cpp
@@ -564,6 +564,30 @@ int main() {
     std::cout << *pFreeValue << '\n';
     */
 
+    // Null value = a special value that means sonething has no value.
+    //              When a pointer is holding a null value,
+    //              that pointer is not pointing at anything (null pointer)
+
+    // nullptr = keyword represents a null literal
+
+    // nullptrs are helpful when determining if an address
+    // was successfully assigned to value.
+
+    /*
+    int *pointer = nullptr;
+    int x = 123;
+
+    pointer = &x;
+
+    if (pointer == nullptr) {
+        std::cout << "address was not assigned" << '\n';
+    }
+    else {
+        std::cout << "address was assigned\n";
+        std::cout << *pointer << '\n';
+    }
+    */
+
     return 0; 
 }
 
