26738a161dfb0253e7b4d8c49678e3464f7881ab
medc-0 <moonisontop76@gmail.com>
2025-11-17 19:27:41 +0100

    docs: learned, grading system with conditionals

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 
 int main() {
-    
-    int age;
 
-    std::cout << "Enter your age: ";
-    std::cin >> age;
+    int gradeP;
 
-    if (age <= 17) {
-        std::cout << "You are a minor." << std::endl;
+    std::cout << "Enter your score: ";
+    std::cin >> gradeP;
+
+    if (gradeP >= 90 && gradeP <= 100) {
+        std::cout << "Your grade is an A!" << '\n';
     }
-    else if (age >= 18 && age <= 64) {
-        std::cout << "You are an Adult." << std::endl;
+    else if (gradeP >= 80 && gradeP <= 89) {
+        std::cout << "Your grade is a B!" << '\n';
+    }
+    else if (gradeP >= 70 && gradeP <= 79) {
+        std::cout << "Your grade is a C.." << '\n';
+    }
+    else if (gradeP >= 60 && gradeP <= 69) {
+        std::cout << "Your grade is a D..." << '\n';
+    }
+    else if (gradeP >= 0 && gradeP <= 59) {
+        std::cout << "Your grade is an F...\nBetter luck next time." << '\n';
     }
     else {
-        std::cout << "You are a senior." << std::endl;
+        std::cout << "Invalid grade points entered." << '\n';
     }
 
+    return 0;
 }
