a112174bb6e30e0830eedd1e7d94fb2a8a9240d8
medc-0 <moonisontop76@gmail.com>
2025-11-17 18:58:56 +0100

    docs: learned, age checking program with if-statements

--- main.cpp
+++ main.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 
 int main() {
+    
+    int age;
 
-    double num1, num2;
-    std::string name;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
 
-    std::cout << "Enter your first number: " << '\n';
-    std::cin >> num1;
+    if (age <= 17) {
+        std::cout << "You are a minor." << std::endl;
+    }
+    else if (age >= 18 && age <= 64) {
+        std::cout << "You are an Adult." << std::endl;
+    }
+    else {
+        std::cout << "You are a senior." << std::endl;
+    }
 
-    std::cout << "Enter your second number: " << '\n';
-    std::cin >> num2;
-
-    std::cout << "Your sum is: " << num1 + num2 << '\n';
-    std::cout << "Your difference is: " << num1 - num2 << '\n';
-    std::cout << "Your result is: " << num1 * num2 << '\n';
-    std::cout << "Your quotient is: " << num1 / num2 << '\n';
-
-    std::cout << "Enter your name: " << '\n';
-    std::cin >> name;
-
-    std::cout << name << ", you just did math like a champ!" << '\n';
-
-    return 0;
 }
