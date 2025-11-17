f843578d204d2b11db422ccbe9251a8154312d94
medc-0 <moonisontop76@gmail.com>
2025-11-17 18:49:50 +0100

    docs: doing some practice programs

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 
-int main()
-{
-    // dynamic memory
-    // polymorphism
-    // OOP
+int main() {
+
+    double num1, num2;
+    std::string name;
+
+    std::cout << "Enter your first number: " << '\n';
+    std::cin >> num1;
+
+    std::cout << "Enter your second number: " << '\n';
+    std::cin >> num2;
+
+    std::cout << "Your sum is: " << num1 + num2 << '\n';
+    std::cout << "Your difference is: " << num1 - num2 << '\n';
+    std::cout << "Your result is: " << num1 * num2 << '\n';
+    std::cout << "Your quotient is: " << num1 / num2 << '\n';
+
+    std::cout << "Enter your name: " << '\n';
+    std::cin >> name;
+
+    std::cout << name << ", you just did math like a champ!" << '\n';
 
     return 0;
 }
